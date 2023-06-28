adac5a7b2fe6b3e4554b7be4eb5f5519465571b4
+ <+>
2023-06-28 09:15:31 +0300

    completed test

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
 const init = () => {
-  
+    const form = document.querySelector('form')
+    form.addEventListener('submit', (e) => {
+        e.preventDefault()
+       //ACCESSING THE INPUT VALUE
+       //e.target.children[1].value
+       //OR
+       const input = document.querySelector('input#searchByID')
+       
+
+       fetch(`http://localhost:3000/movies/${input.value}`)
+       .then (response  => response.json())
+       .then (data => {
+        const title = document.querySelector('section#movieDetails h4')
+        const summary = document.querySelector('section#movieDetails p')
+        title.textContent = data.title
+        summary.textContent = data.summary
+       })
+    })
 }
 
 document.addEventListener('DOMContentLoaded', init);
